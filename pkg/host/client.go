85e76dd7eac34ee6d4fec82ff4a9d1c2d1b7e18e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-24 15:57:13 -0500

    Adds host client and parsing logic
    Introduces functionality to create a client connection to a host.
    
    Adds parsing logic to interpret a string value as a host.

---
+++ pkg/host/client.go
@@ -0,0 +1,10 @@
+package host
+
+import (
+	"github.com/unstoppablemango/ux/pkg/ux"
+	"google.golang.org/grpc"
+)
+
+func NewClient(host ux.Host, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	return host.NewClient(opts...)
+}
