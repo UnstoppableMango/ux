e7309ed7a9412cb875c42b45bfd84926da94dd2d
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-24 13:06:58 -0500

    Move the Input/Source/Sink interfaces into the ux package

--- pkg/cli/input.go
+++ pkg/cli/input.go
@@ -1,39 +1,37 @@
 package cli
 
 import (
-	"context"
-	"io"
+	"iter"
+	"maps"
+
+	"github.com/unstoppablemango/ux/pkg/ux"
 )
 
-type Opener[T any] interface {
-	Open(ctx context.Context) (T, error)
-}
-
-type Source interface {
-	Opener[io.Reader]
-}
-
-type Sink interface {
-	Opener[io.Writer]
-}
-
 type Input struct {
-	Sources map[string]Source
-	Sinks   map[string]Sink
+	Sources map[string]ux.Source
+	Sinks   map[string]ux.Sink
 }
 
-func (i *Input) addSource(key string, source Source) {
+func (i *Input) addSource(key string, source ux.Source) {
 	if i.Sources == nil {
-		i.Sources = map[string]Source{}
+		i.Sources = map[string]ux.Source{}
 	}
 
 	i.Sources[key] = source
 }
 
-func (i *Input) addSink(key string, sink Sink) {
+func (i *Input) addSink(key string, sink ux.Sink) {
 	if i.Sinks == nil {
-		i.Sinks = map[string]Sink{}
+		i.Sinks = map[string]ux.Sink{}
 	}
 
 	i.Sinks[key] = sink
+}
+
+func (i Input) GetSources() iter.Seq2[string, ux.Source] {
+	return maps.All(i.Sources)
+}
+
+func (i Input) GetSinks() iter.Seq2[string, ux.Sink] {
+	return maps.All(i.Sinks)
 }
