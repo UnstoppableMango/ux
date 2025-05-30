d2b1a455eb2429249cb9aa3ffabba251d8422a93
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-05-30 01:37:05 -0500

    New e2e test, who dis

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,12 +1,15 @@
 package e2e_test
 
 import (
+	"context"
 	"os"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
+	uxv1alpha1 "github.com/unstoppablemango/ux/gen/dev/unmango/ux/v1alpha1"
+	"github.com/unstoppablemango/ux/pkg/plugin"
 )
 
 var _ = Describe("E2e", func() {
@@ -17,6 +20,21 @@ var _ = Describe("E2e", func() {
 			ses := Run(cmd)
 
 			Eventually(ses).Should(gexec.Exit(0))
+		})
+	})
+
+	Describe("Dummy", func() {
+		FIt("should return capabilities", func(ctx context.Context) {
+			p := plugin.LocalBinary(dummyPath)
+
+			res, err := p.Capabilities(ctx, &uxv1alpha1.CapabilitiesRequest{})
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(res.All).To(ContainElement(SatisfyAll(
+				HaveField("From", "dummyA"),
+				HaveField("To", "dummyB"),
+				HaveField("Lossy", true),
+			)))
 		})
 	})
 
