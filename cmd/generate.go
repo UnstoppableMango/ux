b31fcd0de1db17e4ebd9d6c9117684d1e2dfbd60
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-01 17:06:03 -0500

    Adds generate command
    Adds a generate command to the CLI.
    
    This command is responsible for generating code based on
    input sources and sinks. It currently logs the names of the
    sources and sinks.

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,24 +1,38 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+	ux "github.com/unstoppablemango/ux/pkg"
+	"github.com/unstoppablemango/ux/pkg/cli"
 )
 
-var generateCmd = NewGenerate()
+type GenerateOptions struct {
+	cli.Options
+}
+
+var generateCmd = NewGenerate(GenerateOptions{})
 
 func init() {
 	rootCmd.AddCommand(generateCmd)
 }
 
-func NewGenerate() *cobra.Command {
-	return &cobra.Command{
+func NewGenerate(options GenerateOptions) *cobra.Command {
+	cmd := &cobra.Command{
 		Use:     "generate",
 		Short:   "Code generation commands",
 		Aliases: []string{"gen"},
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello World!")
+			input, err := cli.Parse(options.Options, args)
+			if err != nil {
+				cli.Fail(err)
+			}
+			if err = ux.Generate(cmd.Context(), input); err != nil {
+				cli.Fail(err)
+			}
 		},
 	}
+
+	cli.Flags(cmd, &options.Options)
+
+	return cmd
 }
