e046b0f6fc9cc5e01ecd02243a09de0af59781d8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-01 12:24:30 -0500

    Plugin capabilities

--- pkg/plugin/registry/capabilities.go
+++ pkg/plugin/registry/capabilities.go
@@ -2,8 +2,12 @@ package registry
 
 import (
 	"context"
+	"maps"
+	"slices"
 
+	"github.com/charmbracelet/log"
 	"github.com/unmango/go/iter"
+	uxv1alpha1 "github.com/unstoppablemango/ux/gen/dev/unmango/ux/v1alpha1"
 	"github.com/unstoppablemango/ux/pkg/plugin"
 	"github.com/unstoppablemango/ux/pkg/plugin/registry/list"
 )
@@ -14,7 +18,14 @@ func Capabilities(ctx context.Context, options ...list.Option) (plugin.CapMap, e
 		return nil, err
 	}
 
-	iter.Map2[]()
+	capabilities := map[string]iter.Seq[*uxv1alpha1.Capability]{}
+	for name, p := range plugins {
+		if res, err := p.Capabilities(ctx, &uxv1alpha1.CapabilitiesRequest{}); err != nil {
+			log.Debug("Failed to look up capabilities", "name", name, "err", err)
+		} else {
+			capabilities[name] = slices.Values(res.All)
+		}
+	}
 
-	return nil, nil
+	return maps.All(capabilities), nil
 }
