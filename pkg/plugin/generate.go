0aab6fb90e7cab44e535a757c3d9e0aebb00f530
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-11 13:20:37 -0500

    Adds filesystem server to plugin execution
    Adds a filesystem server to the plugin execution process, allowing plugins to interact with a virtual file system.
    
    This change enables more complex plugin interactions by providing a way for plugins to read and write files, which is essential for certain generation tasks. The filesystem server is implemented using gRPC and a memory-backed file system.
    
    The changes include:
    - Starting a gRPC server that implements the protofs interface.
    - Connecting to this server from the dummy plugin.
    - Writing a test file to the virtual filesystem.
    
    This setup allows for testing plugin functionality without relying on the local filesystem.

--- pkg/plugin/generate.go
+++ pkg/plugin/generate.go
@@ -2,6 +2,8 @@ package plugin
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	filev1alpha1 "buf.build/gen/go/unmango/protofs/protocolbuffers/go/dev/unmango/file/v1alpha1"
 	"github.com/charmbracelet/log"
@@ -23,32 +25,46 @@ func Generate(ctx context.Context, name string, input ux.Input) (afero.Fs, error
 		})
 	}
 
+	log.Info("Creating FS listener")
 	lis, err := fs.Listen(ctx)
 	if err != nil {
 		return nil, err
 	}
 
+	log.Info("Starting FS server")
 	output := afero.NewMemMapFs()
 	srv := fs.NewServer(serverFs(input, output))
 	defer srv.GracefulStop()
 
 	go func() {
+		log.Info("Serving FS")
 		if err := srv.Serve(lis); err != nil {
-			log.Error(err)
+			log.Info("FS Server returned an error", "err", err)
 		}
 	}()
 
+	time.Sleep(500 * time.Microsecond)
+	log.Info("Sending generate request")
 	id := uuid.NewString()
 	res, err := plugin.Generate(ctx, &uxv1alpha1.GenerateRequest{
 		Id:        id,
 		Inputs:    inputs,
-		FsAddress: lis.Addr().String(),
+		FsAddress: fmt.Sprintf("unix://%s", lis.Addr()),
 	})
 	if err != nil {
 		return nil, err
 	}
 
 	log.Info("Got outputs", "files", res.Outputs)
+	err = afero.Walk(output, "", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		} else {
+			log.Info("Found output", "path", path, "name", info.Name())
+			return nil
+		}
+	})
+
 	return output, err
 }
 
