012fea72229e5f52d6812f3643e5ad9e85f395a4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-25 17:47:08 -0500

    Delete all the input stuff

--- pkg/cli/options.go
+++ pkg/cli/options.go
@@ -1,49 +1,12 @@
 package cli
 
 import (
-	"iter"
-	"maps"
-
 	"github.com/spf13/cobra"
-	ux "github.com/unstoppablemango/ux/pkg"
-	"github.com/unstoppablemango/ux/pkg/cli/sink"
-	"github.com/unstoppablemango/ux/pkg/cli/source"
 )
-
-type OptionsParser interface {
-	Sinks() (iter.Seq2[string, ux.Sink], error)
-	Sources() (iter.Seq2[string, ux.Source], error)
-}
 
 type Options struct {
 	Inputs  []string
 	Outputs []string
-}
-
-func (opts Options) Sinks() (iter.Seq2[string, ux.Sink], error) {
-	sinks := map[string]ux.Sink{}
-	for _, o := range opts.Outputs {
-		if s, err := sink.Parse(o); err != nil {
-			return nil, err
-		} else {
-			sinks[o] = s
-		}
-	}
-
-	return maps.All(sinks), nil
-}
-
-func (opts Options) Sources() (iter.Seq2[string, ux.Source], error) {
-	sources := map[string]ux.Source{}
-	for _, i := range opts.Inputs {
-		if s, err := source.Parse(i); err != nil {
-			return nil, err
-		} else {
-			sources[i] = s
-		}
-	}
-
-	return maps.All(sources), nil
 }
 
 func Flags(cmd *cobra.Command, opts *Options) {
