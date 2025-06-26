921b3d94f25bcaf88c4656e9c97bbacffa152515
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-25 20:40:58 -0500

    Delete all the input stuff (#81)

--- pkg/cli/sink/parse.go
+++
@@ -1,7 +0,0 @@
-package sink
-
-import ux "github.com/unstoppablemango/ux/pkg"
-
-func Parse(o string) (ux.Sink, error) {
-	return File(o), nil
-}
--- pkg/cli/source/parse.go
+++
@@ -1,7 +0,0 @@
-package source
-
-import ux "github.com/unstoppablemango/ux/pkg"
-
-func Parse(i string) (ux.Source, error) {
-	return File(i), nil
-}
