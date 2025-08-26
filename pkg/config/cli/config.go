73e34a4c6c94920c4c3eace21e56fa22a041de8a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-08-26 09:50:33 -0500

    Fleshing out the ux side

--- pkg/config/cli/config.go
+++ pkg/config/cli/config.go
@@ -1,11 +1,64 @@
 package cli
 
 import (
-	"github.com/spf13/cobra"
+	"context"
+	"io"
+
 	ux "github.com/unstoppablemango/ux/pkg"
 )
 
-type Builder cobra.Command
+type Argument struct {
+	name    string
+	aliases []string
+}
 
-func (b *Builder) Configure(c ux.Configure) {
+// Alias implements ux.Option.
+func (b *Argument) Alias(v string) {
+	b.aliases = append(b.aliases, v)
+}
+
+// Name implements ux.Option.
+func (b *Argument) Name(v string) {
+	b.name = v
+}
+
+type Builder struct{
+	inputs map[string]*Argument
+}
+
+// Input implements ux.Builder.
+func (b *Builder) Input(c ux.Configure) string {
+	opt := &Argument{}
+	c.Configure(opt)
+	b.inputs[opt.name] = opt
+	return opt.name
+}
+
+type Context struct{
+	inputs map[string]*Argument
+}
+
+// Context implements ux.Context.
+func (c *Context) Context() context.Context {
+	return context.TODO()
+}
+
+// Input implements ux.Context.
+func (c *Context) Input(name string) (io.Reader, error) {
+	panic("unimplemented")
+}
+
+// Output implements ux.Context.
+func (c *Context) Output(name string) (io.Writer, error) {
+	panic("unimplemented")
+}
+
+func Execute(g ux.Generator) error {
+	builder := &Builder{}
+	if err := g.Configure(builder); err != nil {
+		return err
+	}
+
+	ctx := &Context{builder.inputs}
+	return g.Generate(ctx)
 }
