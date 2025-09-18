4ca70cc409f3b193ad4e7e44d7032230c23e4a0c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-09-17 23:04:50 -0500

    WIP

--- pkg/plugin/skel/skel.go
+++ pkg/plugin/skel/skel.go
@@ -5,9 +5,38 @@ import (
 	"io"
 	"os"
 
+	"github.com/unmango/go/cli"
+	mangos "github.com/unmango/go/os"
 	uxv1alpha1 "github.com/unstoppablemango/ux/gen/dev/unmango/ux/v1alpha1"
 	"google.golang.org/protobuf/proto"
 )
+
+type CmdArgs struct {
+	Args      []string
+	StdinData []byte
+}
+
+type UxFuncs struct {
+	Generate func(*CmdArgs) error
+}
+
+func (funcs UxFuncs) execute(ctx context.Context, mos mangos.Os) error {
+	stdin, err := io.ReadAll(mos.Stdin())
+	if err != nil {
+		return err
+	}
+
+	args := &CmdArgs{
+		Args:      os.Args,
+		StdinData: stdin,
+	}
+
+	if funcs.Generate != nil {
+		return funcs.Generate(args)
+	} else {
+		return nil
+	}
+}
 
 type Cli struct {
 	Generate func(context.Context, []string) error
@@ -33,5 +62,12 @@ func (p *Cli) ExecuteContext(ctx context.Context) error {
 		return p.Generate(ctx, stdin.Args)
 	default:
 		return nil
+	}
+}
+
+func PluginMain(funcs UxFuncs) {
+	ctx := context.Background()
+	if err := funcs.execute(ctx, mangos.System); err != nil {
+		cli.Fail(err)
 	}
 }
