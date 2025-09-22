0cf608f3d65a47d3d48d9aa7fc41e491973d0b02
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-09-21 21:58:57 -0500

    Plugin searching

--- cmd/plugin/search.go
+++ cmd/plugin/search.go
@@ -3,6 +3,7 @@ package plugin
 import (
 	"fmt"
 
+	"github.com/sahilm/fuzzy"
 	"github.com/spf13/cobra"
 	"github.com/unstoppablemango/ux/pkg/cli"
 	"github.com/unstoppablemango/ux/pkg/work"
@@ -13,14 +14,20 @@ func NewSearch() *cobra.Command {
 		Use:     "search [PATTERN]",
 		Short:   "Search for plugins fuzzy matching PATTERN",
 		Aliases: []string{"find"},
+		Args:    cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			work, err := work.Cwd()
 			if err != nil {
 				cli.Fail(err)
 			}
 
+			var plugins []string
 			for p := range work.Plugins() {
-				s := fmt.Sprint(p)
+				plugins = append(plugins, fmt.Sprint(p))
+			}
+
+			for _, m := range fuzzy.Find(args[0], plugins) {
+				fmt.Println(m.Str)
 			}
 		},
 	}
