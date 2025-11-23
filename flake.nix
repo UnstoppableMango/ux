d3d47eb737a3913c10db2944c509a0c5d3c4c2c8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-11-22 23:23:09 -0600

    Flake parts (#179)

--- flake.nix
+++ flake.nix
@@ -3,13 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     gomod2nix = {
       url = "github:nix-community/gomod2nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
@@ -18,46 +15,47 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      gomod2nix,
-      treefmt-nix,
-    }:
-    (flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        ux = pkgs.callPackage ./. {
-          inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
-        };
-        uxApp = {
-          type = "app";
-          program = ux + "/bin/ux";
-          meta = ux.meta;
-        };
-      in
-      {
-        formatter = treefmt-nix.lib.mkWrapper pkgs {
-          projectRootFile = "flake.nix";
-          programs = {
-            nixfmt.enable = true;
-            dprint.enable = true;
-            gofmt.enable = true;
-            buf.enable = true;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      imports = [ inputs.treefmt-nix.flakeModule ];
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          ux = pkgs.callPackage ./. {
+            inherit (inputs.gomod2nix.legacyPackages.${system}) buildGoApplication;
+          };
+          uxApp = {
+            type = "app";
+            program = ux + "/bin/ux";
+            meta = ux.meta;
+          };
+        in
+        {
+          packages.ux = ux;
+          packages.default = ux;
+
+          apps.ux = uxApp;
+          apps.default = uxApp;
+
+          devShells.default = pkgs.callPackage ./shell.nix {
+            inherit (inputs.gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              # dprint.enable = true;
+              gofmt.enable = true;
+              buf.enable = true;
+            };
           };
         };
-
-        packages.ux = ux;
-        packages.default = ux;
-
-        apps.ux = uxApp;
-        apps.default = uxApp;
-
-        devShells.default = pkgs.callPackage ./shell.nix {
-          inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
-        };
-      }
-    ));
+    };
 }
