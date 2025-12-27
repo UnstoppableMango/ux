795fcb6b3e9d2cfea2c479b81a68e5eb11674bcb
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-12-27 17:26:03 -0600

    Rename test file

---
+++ test/e2e/generate_test.go
@@ -0,0 +1,26 @@
+package e2e_test
+
+import (
+	"os/exec"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("Generate", func() {
+	It("should generate Go code from an OpenAPI spec", Pending, func() {
+		tmp := GinkgoT().TempDir()
+		out := filepath.Join(tmp, "petstore.go")
+		CopyTestdata(tmp)
+
+		cmd := exec.Command(uxPath, "gen", "go", "petstore.yml")
+		cmd.Dir = tmp
+
+		ses := Run(cmd)
+
+		Eventually(ses).Should(gexec.Exit(0))
+		Expect(out).To(BeARegularFile())
+	})
+})
