83b6533fd5d70816e4b0efae7bb9b538848f6c3d
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-12-27 22:23:03 -0600

    WIP

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,6 +4,7 @@ import (
 	"path/filepath"
 
 	"github.com/adrg/xdg"
+	"github.com/spf13/viper"
 )
 
 const (
@@ -20,4 +21,39 @@ var (
 	LocalBin    = xdg.BinHome
 )
 
-type Config interface{}
+type Target struct {
+	Type    string   `json:"type" yaml:"type"`
+	Args    []string `json:"args" yaml:"args"`
+	Command []string `json:"command" yaml:"command"`
+	Outputs []string `json:"outputs" yaml:"outputs"`
+}
+
+type Config struct {
+	Targets map[string]Target `json:"targets" yaml:"targets"`
+}
+
+func NewViper() *viper.Viper {
+	v := viper.New()
+	v.SetConfigName(DefaultName)
+	v.AddConfigPath(DefaultDir)
+	v.AddConfigPath(".")
+
+	return v
+}
+
+func Read(v *viper.Viper) (*Config, error) {
+	if v == nil {
+		v = NewViper()
+	}
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := v.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
