585907df2c5a0bddf5623a12541a26beb6789490
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-01-06 22:30:09 -0600

    WIP

--- pkg/work/work.go
+++ pkg/work/work.go
@@ -1,1 +1,29 @@
 package work
+
+import (
+	"fmt"
+
+	"github.com/spf13/afero"
+)
+
+type Space interface {
+	afero.Fs
+}
+
+type space struct {
+	afero.Fs
+}
+
+func DirFs(dir string, fs afero.Fs) (Space, error) {
+	if info, err := fs.Stat(dir); err != nil {
+		return nil, fmt.Errorf("opening workspace: %w", err)
+	} else if !info.IsDir() {
+		return nil, fmt.Errorf("workspace is not a directory")
+	}
+
+	return &space{Fs: afero.NewBasePathFs(fs, dir)}, nil
+}
+
+func Dir(dir string) (Space, error) {
+	return DirFs(dir, afero.NewOsFs())
+}
