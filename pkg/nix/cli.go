ee26b55f1e709c4ce50538b23ea29860c9aa65d5
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-05-10 01:28:48 -0500

    WIP

--- pkg/nix/cli.go
+++ pkg/nix/cli.go
@@ -10,17 +10,6 @@ import (
 	"github.com/unstoppablemango/ux/internal"
 )
 
-type (
-	Result              = nixv1alpha1.Result
-	CommonOptions       = nixv1alpha1.CommonOptions
-	BuildRequest        = nixv1alpha1.BuildRequest
-	BuildResponse       = nixv1alpha1.BuildResponse
-	InstantiateRequest  = nixv1alpha1.InstantiateRequest
-	InstantiateResponse = nixv1alpha1.InstantiateResponse
-	StoreRequest        = nixv1alpha1.StoreRequest
-	StoreResponse       = nixv1alpha1.StoreResponse
-)
-
 type Cli struct {
 	nixv1alpha1.UnimplementedNixServiceServer
 }
--- pkg/types.go
+++ pkg/types.go
@@ -6,7 +6,6 @@ type (
 	Config         = uxv1alpha1.Config
 	Repo           = uxv1alpha1.Repo
 	Derivation     = uxv1alpha1.Derivation
-	Package        = uxv1alpha1.Package
 	InvokeRequest  = uxv1alpha1.InvokeRequest
 	InvokeResponse = uxv1alpha1.InvokeResponse
 	Link           = uxv1alpha1.Link
